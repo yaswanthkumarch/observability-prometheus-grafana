782affe790278ddcf82f17b1b2a626a23e4cff68
yaswanth <82691830+yaswanthkumarch@users.noreply.github.com>
2025-07-27 21:35:44 +0530

    Update NotFound.js

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -5,7 +5,7 @@ import logo from '../logo.svg';
 function NotFound() {
   return (
     <div className="container fade-in not-found">
-      <img src={logo} alt="DevOps Shack logo" className="logo" />
+   
       <h2>404 - Page Not Found</h2>
       <p>The page you are looking for does not exist.</p>
       <Link to="/login">
